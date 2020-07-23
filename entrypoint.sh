8abfe72283a7e159ece0fcf3bd9bf2003a04eece
Jake Sylvestre <jakesyl@gmail.com>
2020-07-23 15:53:49 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ send_comment() {
 	PAYLOAD=$(echo '{}' | jq --arg body "${COMMENT}" '.body = $body')
 	COMMENTS_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .commits_url)$(echo "/comments")
 	echo $COMMENTS_URL
-	curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" > /dev/null
+	curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" 
 }
 
 # mod_download is getting go modules using go.mod.
