04e0014b30ab8364867c7119dee0b5354a77a22a
Jake Sylvestre <jakesyl@gmail.com>
2020-07-23 15:46:10 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,8 @@ SUCCESS=0
 # this function use ${GITHUB_TOKEN}, ${COMMENT} and ${GITHUB_EVENT_PATH}
 send_comment() {
 	PAYLOAD=$(echo '{}' | jq --arg body "${COMMENT}" '.body = $body')
-	COMMENTS_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .comments_url)
+	COMMENTS_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .commits_url)$(echo "/comments")
+	echo $COMMENTS_URL
 	curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" > /dev/null
 }
 
